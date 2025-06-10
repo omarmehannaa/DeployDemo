0be3bd1ef55290aae9985846f1c9ccdd278890e8
Omar <omarmehana15@gmail.com>
2025-06-10 03:38:42 +0300

    Add project files.

---
+++ Controllers/Test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HelloWorld()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
